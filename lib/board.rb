0e4c31665a5112961200c767113b02c3553fec70
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-11 16:38:12 -0400

    Add draw method

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,11 @@ class Board
   def initialize
     @grid = Array.new(3) { Array.new(3, ' ') }
   end
+
+  def draw
+    @grid.each_with_index do |row, index|
+      puts " #{row[0]} | #{row[1]} | #{row[2]}"
+      puts '---+---+---' if index != 2
+    end
+  end
 end
