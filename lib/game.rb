c415c4cd57d284333c9fcb01be97cb790fd7e0dc
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-14 17:55:19 -0400

    Add initialize method

--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,10 @@ require_relative 'board'
 require_relative 'player'
 
 class Game
+  def initialize
+    @winner = nil
+    @board = Board.new
+    @player1 = Player.new('x', @board)
+    @player2 = Player.new('o', @board)
+  end
 end
