ebc8b7e2b63cc793308fcd6f337c1fc5ff463203
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-16 16:42:33 -0400

    Update initialize method to set player names

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,11 @@ require_relative 'player'
 class Game
   attr_reader :winner
 
-  def initialize
+  def initialize(name1, name2)
     @winner = nil
     @board = Board.new
-    @player1 = Player.new('x', @board)
-    @player2 = Player.new('o', @board)
+    @player1 = Player.new(name1, 'x', @board)
+    @player2 = Player.new(name2, 'o', @board)
+    @current_player = @player1
   end
 end
