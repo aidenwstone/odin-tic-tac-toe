e381a666fe5a46ac1ecb1d89f78ae403ad6c1147
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-21 16:13:47 -0400

    Add frozen string literal comments

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Board
   def initialize
     @grid = Array.new(3) { Array.new(3, ' ') }
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'lib/game'
 
 game = Game.new('Player 1', 'Player 2')
